5742d501536a456a48a5e6baef719b52380e44ed
Rajdeep Mitra <rajdeepmitra8@gmail.com>
2024-05-04 11:27:27 +0530

    Made improvements

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,19 +5,20 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store, { persistor } from "./Redux-Toolkit/store.js";
 import { PersistGate } from "redux-persist/integration/react";
+import App from "./App.jsx";
 // import("./App.jsx")
-const LazyLoadedComponent = React.lazy(() => {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(import("./App.jsx")), 3000); // Add a 3s delay
-  });
-});
+// const LazyLoadedComponent = React.lazy(() => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve(import("./App.jsx")), 3000); // Add a 3s delay
+//   });
+// });
 const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <Suspense
+          {/* <Suspense
             fallback={
               <div className="flex justify-center items-center h-screen text-2xl font-semibold bg-black">
                 <span className="loader"></span>
@@ -25,7 +26,8 @@ root.render(
             }
           >
             <LazyLoadedComponent />
-          </Suspense>
+          </Suspense> */}
+          <App />
         </PersistGate>
       </Provider>
     </BrowserRouter>

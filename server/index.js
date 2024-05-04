92fbfbe924a1e7fcf09a82f2b14220fc03b01761
Rajdeep Mitra <rajdeepmitra8@gmail.com>
2024-05-04 12:17:48 +0530

    Made improvements

--- server/index.js
+++ server/index.js
@@ -13,18 +13,35 @@ function startServer() {
   app.use(express.json()); // Parse JSON bodies
   app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
   app.use(cookieParser()); // Use cookie-parser middleware
+
   // Custom CORS middleware function
   const handleCorsError = (req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173"); // Allow requests from any origin
+    // List of allowed origins separated by commas
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "https://job-finder-web-nmqp5m4vw-mitrarajdeep2001s-projects.vercel.app",
+      // Add more origins as needed
+    ];
+
+    // Get the request origin
+    const requestOrigin = req.headers.origin;
+
+    // Check if the request origin is in the list of allowed origins
+    if (allowedOrigins.includes(requestOrigin)) {
+      // Set the Access-Control-Allow-Origin header to the request origin
+      res.setHeader("Access-Control-Allow-Origin", requestOrigin);
+    }
+
+    // Set other CORS headers
     res.setHeader(
       "Access-Control-Allow-Methods",
       "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-    ); // Allowed HTTP methods
+    );
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Content-Type, Authorization"
-    ); // Allowed headers
-    res.setHeader("Access-Control-Allow-Credentials", "true"); // Allow credentials
+    );
+    res.setHeader("Access-Control-Allow-Credentials", "true");
 
     // Check if it's a preflight request
     if (req.method === "OPTIONS") {
